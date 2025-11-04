97f99e601f7b8dfe0ec05fea897f2479141b935c
Timur <trybakov@gmail.com>
2025-11-05 02:15:14 +0300

    Opensearch integration part 2

--- KafkaOpenTelemetryLogger.AppHost/AppHost.cs
+++ KafkaOpenTelemetryLogger.AppHost/AppHost.cs
@@ -1,10 +1,9 @@
-﻿using System.Net.Sockets;
-using Aspire.Hosting;
+﻿using Aspire.Hosting;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var seqPassword = builder.AddParameter("seq-admin-password", "test", secret: true);
+var seqPassword = builder.AddParameter("seq-admin-password", "test", secret: true).ExcludeFromManifest();
 var seqApiKey = "devseqkey12345678";
 
 var seq = builder.AddSeq("seq", seqPassword, port: 5341)
@@ -16,6 +15,26 @@ var kafka = builder.AddKafka("kafka", port: 9092)
     .WithDataVolume("otlp-kafka", isReadOnly: false)
     .WithLifetime(ContainerLifetime.Persistent);
 
+var openSearchPassword = "I_j63c9fa2392a9568ee41";
+var openSearch = builder.AddContainer("opensearch", "opensearchproject/opensearch", "3.3.2")
+    .WithEnvironment("cluster.name", "opensearch-dev")
+    .WithEnvironment("node.name", "opensearch-node")
+    .WithEnvironment("discovery.type", "single-node")
+    .WithEnvironment("bootstrap.memory_lock", "true")
+    .WithEnvironment("OPENSEARCH_JAVA_OPTS", "-Xms512m -Xmx512m")
+    .WithEnvironment("OPENSEARCH_INITIAL_ADMIN_PASSWORD", openSearchPassword)
+    //.WithBindMount("opensearch-data", "/usr/share/opensearch/data")
+    //.WithBindMount("./opensearch.yml", "/usr/share/opensearch/config/opensearch.yml")
+    .WithEnvironment("cluster.routing.allocation.disk.threshold_enabled", "false") // ← работает
+    .WithEndpoint(port: 9200, targetPort: 9200, name: "http")
+    .WithEndpoint(port: 9600, targetPort: 9600, name: "perf")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var openSearchDashboard = builder.AddContainer("opensearch-dashboards", "opensearchproject/opensearch-dashboards", "3.3.0")
+    .WithHttpEndpoint(port: 5601, targetPort: 5601)
+    .WithEnvironment("OPENSEARCH_HOSTS", "[\"https://opensearch:9200\"]")
+    .WithLifetime(ContainerLifetime.Persistent);
+
 var collector = builder.AddContainer("otel-collector", "otel/opentelemetry-collector-contrib", "0.138.0")
     .WithHttpEndpoint(targetPort: 4318, name: "otlp-http")
     .WithEndpoint(targetPort: 4317, name: "otlp-grpc")
@@ -23,27 +42,13 @@ var collector = builder.AddContainer("otel-collector", "otel/opentelemetry-colle
     .WithReference(seq)
     .WithReference(kafka)
     .WaitFor(kafka)
+    .WaitFor(openSearch)
     .WithEnvironment("KAFKA_BROKERS", "kafka:9093")
     .WithEnvironment("SEQ_ENDPOINT", "http://seq:5341/ingest/otlp")
     .WithEnvironment("SEQ_API_KEY", seqApiKey)
+    .WithEnvironment("OPENSEARCH_USERNAME", "admin")
+    .WithEnvironment("OPENSEARCH_PASSWORD", openSearchPassword)
     .WithArgs("--config", "/etc/otelcol-config.yaml");
-
-//var openSearchPassword = builder.AddParameter("seq-admin-password", "test", secret: true);
-var openSearch = builder.AddContainer("opensearch", "opensearchproject/opensearch", "3.3.2")
-    .WithEnvironment("cluster.name", "opensearch-dev")
-    .WithEnvironment("node.name", "opensearch-node")
-    .WithEnvironment("discovery.type", "single-node") // ← важно для одной ноды
-    .WithEnvironment("bootstrap.memory_lock", "true")
-    .WithEnvironment("OPENSEARCH_JAVA_OPTS", "-Xms512m -Xmx512m")
-    .WithEnvironment("OPENSEARCH_INITIAL_ADMIN_PASSWORD", "test")
-    .WithBindMount("opensearch-data", "/usr/share/opensearch/data")
-    .WithEndpoint(port: 9200, targetPort: 9200, name: "http")
-    .WithEndpoint(port: 9600, targetPort: 9600, name: "perf")
-    .WithLifetime(ContainerLifetime.Persistent);
-
-builder.AddContainer("opensearch-dashboards", "opensearchproject/opensearch-dashboards", "2.19.3")
-    .WithEndpoint(port: 5601, targetPort: 5601)
-    .WithEnvironment("OPENSEARCH_HOSTS", "[\"https://opensearch:9200\"]");
 
 var aService = builder.AddProject<KafkaOpenTelemetryLogger_AService>("a-service")
     .WithEnvironment(context =>
